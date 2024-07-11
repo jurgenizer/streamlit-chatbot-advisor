95f28f0aeb3f98c7f5c13c28b44c574afa736484
Jurgen Geitner <jurgenizer@users.noreply.github.com>
2024-07-11 09:48:45 +0000

    remove updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+
 
 # Show title and description.
 st.title("💬 adlicious advisor")
